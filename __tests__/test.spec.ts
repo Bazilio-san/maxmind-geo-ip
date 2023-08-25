bd6ca238db2b0757a72c0a0fbafde3dd1032c7ac
Bazilio-san <github@bazilio.ru>
2023-08-26 02:52:44 +0300

    2.0.20

--- __tests__/test.spec.ts
+++ __tests__/test.spec.ts
@@ -3,18 +3,20 @@ import { IMaxMindConfig, startGeoIP } from '../src';
 
 export const config: IMaxMindConfig = configModule.util.toObject();
 
+const expected = {
+  continent: 'Asia',
+  continentCode: 'AS',
+  country: 'Indonesia',
+  countryISOCode: 'ID',
+  city: undefined,
+  latitude: -6.1728,
+  longitude: 106.8272,
+  postalCode: undefined,
+  subdivision: undefined,
+};
+
 test(`The database must be initialized and work correctly`, async () => {
   const geoIp = await startGeoIP(config.maxMind);
   const result = geoIp?.lookupEx('103.100.173.193', 'en');
-  expect(result).toEqual({
-    continent: 'Asia',
-    continentCode: 'AS',
-    country: 'Indonesia',
-    countryISOCode: 'ID',
-    city: undefined,
-    latitude: -6.1728,
-    longitude: 106.8272,
-    postalCode: undefined,
-    subdivision: undefined,
-  });
+  expect(result).toEqual(expected);
 });
